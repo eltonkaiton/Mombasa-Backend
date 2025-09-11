5c6fe1a4c9904428b266195d657f9aa6c56586e8
eltonkaiton <eltonkaiton@gmail.com>
2025-09-11 20:38:51 +0300

    005 uri updates

--- utilis/db.js
+++ utilis/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://kaiton254:@kaiton254@cluster0.ytmxwnj.mongodb.net/Mombasa-Database?retryWrites=true&w=majority&appName=Cluster0');
+    await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://kaiton254:kaiton254@cluster0.ytmxwnj.mongodb.net/Mombasa-Database?retryWrites=true&w=majority&appName=Cluster0');
     console.log('✅ MongoDB connected');
   } catch (err) {
     console.error('❌ MongoDB connection error:', err);
